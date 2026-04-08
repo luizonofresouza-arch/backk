e7d1c5e73cd2febef64a7a6452b5cfc6987f53c9
luizonofresouza-arch <luiz.onofre.souza@escola.pr.gov.br>
2026-04-08 11:04:50 -0300

    Add files via upload

---
+++ luiz.js
@@ -0,0 +1,14 @@
+//1.
+console.log("olá mundo");
+//2.
+let nome = "Luiz";
+console.log(nome);
+//3.
+let soma = 5 + 3;
+console.log(soma);
+//4.
+let subtrair = 5-2;
+console.log(subttrair);
+//5.
+let multiplique = 5 * 8
+console.log(mutiplique)
